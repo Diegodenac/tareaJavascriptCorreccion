929942f45b17820856c6a3017abf0766aa90e22a
Diegodenac <diegocanedo917@gmail.com>
2024-02-15 16:04:58 -0500

    Multiplicador funcionando

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const firstFactor = document.querySelector("#primer-numeroM");
+const secondFactor = document.querySelector("#segundo-numeroM");
+const formMultiplicar = document.querySelector("#multiplicar-form");
+const divProducto = document.querySelector("#resultadoM-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +18,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+formMultiplicar.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstFactor.value);
+  const secondNumber = Number.parseInt(secondFactor.value);
+
+  divProducto.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
